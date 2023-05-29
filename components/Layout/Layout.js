17e720a9d4a7f4172b895a665575dc4528558178
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-05-29 16:25:26 +0330

    create header & footer

---
+++ components/Layout/Layout.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import styles from '../Layout/Layout.module.css';
+
+const Layout = ({children}) => {
+  return (
+    <>
+      <Link href='/'>
+      <header className={styles.header}>
+        <h2>Car Expo</h2>
+        <p>Choose and Buy your Car</p>
+      </header>
+      </Link>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>
+        <a href="https://github.com/jamalshamsi1988/auto-expo" target="_blank">
+            Jamal Shamsi Next.js | Car shope project &copy;
+        </a>
+      </footer>
+    </>
+  )
+}
+
+export default Layout
