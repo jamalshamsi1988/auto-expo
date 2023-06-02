6b34753b186f3c84b3d56aa83597af69e3fae5a1
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-06-02 16:05:52 +0330

    create SearchBar component with css styles

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,10 +1,12 @@
 import Categories from '../../components/modules/Categories';
+import SearchBar from '../../components/modules/SearchBar';
 import carsData from '../../data/carsData';
 import CarsPage from './../../components/templates/CarsPage';
 
 const Detailes = () => {
   return (
     <div >
+      <SearchBar />
       <Categories />
       <CarsPage data={carsData}  />
     </div>
